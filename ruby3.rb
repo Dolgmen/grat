cf1a999aa464beead88f708c7f220e9e3ae54407
Dolgmen <slotsy046@gmail.com>
2021-03-29 21:39:14 +0300

    ruby3.rb

---
+++ ruby3.rb
@@ -0,0 +1,18 @@
+x = 165
+y = 111
+res = x + y
+res = x - y
+res = x * y
+res = x / y
+res = 2 ** 3
+puts res
+res = x / y
+puts res
+puts ("results of divide:" + res.to_s)
+
+num = -14.45
+puts (num.abs())
+puts (num.round())
+puts Math.sqrt(144)
+puts (Math.log(145).round())
+puts
